fe01115e080169e1c3d97414675ee2c0c72db37e
Samuel Park <sam.park.email@gmail.com>
2021-09-01 19:08:47 -0400

    added filter and map to dynamically display info

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
+import activities from './data'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // const [{ name, type }] = activities
+// console.log(activity)
+  const [filtered, setFilterComponent] = useState('All')
+  // console.log(filter)
+    return (
+      <>
+      <button onClick={() => setFilterComponent('All')}>All</button>
+      <button onClick={() => setFilterComponent('Information')}>Information</button>
+      <button onClick={() => setFilterComponent('Games')}>Games</button>
+      <button onClick={() => setFilterComponent('Video')}>Video</button>
+      {filtered === 'All' ? (<div>{activities.filter(active => active.type === 'All').map(thing => 
+        (<div key={thing.id}>{thing.name}</div>))}</div>) : <></> }
+      {/* {filtered === 'All' ? (<div>{activities.filter(type => type === 'All').map(filteredType => {
+       (
+          <div key={filteredType.name}>{filteredType}a aaaaa</div> 
+
+        )
+      })
+      } </div> ) : 'not' } */}
+      {filtered === 'Information' ? (<div> Hello is this Information </div>) : '' }
+      </>
+    );
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,49 @@
+// 1. Box Breathing                     Games     
+
+// 2.Calm Counting                     Games
+
+// 3. Decoding Messages           Games
+
+// 4. Going on an Adventure       Games
+
+// 5. Match the Color                   Games
+
+// 6. Superhero Workout             Games
+
+// 7. 5-4-3-2-1 Technique             Games
+
+// 8. Healthy Habits                     Informational
+
+// 9. How to do the Five              Informational
+
+// 10. How to wash your hands   Informational
+
+// 11. Milk, Milk, Milk                     Games
+
+// 12. Hokey Pokey                       Video
+
+// 13. Jumping Jacks                   Informational
+
+// 14. Pushups                            Informational
+
+// 15. What is COVID-19?           Video
+
+const activities = [{name:'Box Breathing', type:'All', id: 1}, 
+                {name:'Calm Counting', type:'Games', id: 2},
+                {name:'Decoding Messages ', type:'Games', id: 3}, 
+                {name:'Going on an Adventure', type:'Games', id: 4}, 
+                {name:'Match the Color', type:'Games', id: 5}, 
+                {name:'Box Breathing', type:'All', id: 1}, 
+                {name:'Box Breathing', type:'All', id: 1}, 
+                {name:'Box Breathing', type:'All', id: 1}, 
+                {name:'Box Breathing', type:'All', id: 1}, 
+                {name:'Box Breathing', type:'All', id: 1}, 
+                {name:'Box Breathing', type:'All', id: 1}, 
+                {name:'Box Breathing', type:'All', id: 1}, 
+                {name:'Box Breathing', type:'All', id: 1}, 
+                {name:'Box Breathing', type:'All', id: 1}, 
+                {name:'Box Breathing', type:'All', id: 1}, 
+                {name:'Box Breathing', type:'All', id: 1}, 
+            ]
+
+export default activities
