8fbdfde6c4789297fb5825a374869b5940bdf4d1
Samuel Park <sam.park.email@gmail.com>
2021-09-02 00:37:05 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -1,37 +1,79 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
+import { ButtonGroup } from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles'
 import './App.css';
 import activities from './data'
+import Button from '@material-ui/core/Button';
+import Header from './components/Header'
+
+
+
+const button = makeStyles({
+  root: {
+    background: 'linear-gradient(45deg, #8559A0 30%, #8559A0 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '3px 5px 2px rgba(133, 89, 160, .3)',
+    color: 'white',
+    height: 38,
+    padding: '10px 30px',
+    margin: '10px',
+    '&:hover': {
+      background: 'linear-gradient(45deg, #8559A0 30%, #c383eb 90%)',
+      boxShadow: '3px 5px 2px rgba(133, 89, 160, .5)',
+    },
+    '&:focus': {
+      background: '#4a1b9a',
+      boxShadow: '3px 5px 2px rgba(133, 89, 160, .5)',
+    },
+  },
+});
+
 
 function App() {
-  // const [{ name, type }] = activities
-// console.log(activity)
+  const classes = button();
+
   const [filtered, setFilterComponent] = useState('All')
-  // console.log(filter)
+
     return (
-      <>
-      <button onClick={() => setFilterComponent('All')}>All</button>
-      <button onClick={() => setFilterComponent('Informational')}>Information</button>
-      <button onClick={() => setFilterComponent('Games')}>Games</button>
-      <button onClick={() => setFilterComponent('Video')}>Video</button>
+      <div className='container'>
+        <Header />
+     <div className='button-container'>
+
+      <ButtonGroup>
+        <Button className={classes.root} rounded='true' onClick={() => setFilterComponent('All')}>All</Button>
+        <Button className={classes.root} rounded='true' onClick={() => setFilterComponent('Informational')}>Informational</Button>
+        <Button className={classes.root} onClick={() => setFilterComponent('Games')}>Games</Button>
+        <Button className={classes.root} onClick={() => setFilterComponent('Video')}>Video</Button>
+      </ButtonGroup>
+     </div>
+     <div className='activities-container'>
+
+      {/* show all activites */}
       {filtered === 'All' ? (<div>{activities.map(item => 
-        (<div key={item.id}>{item.name}</div>))}</div>) : <></> }
+        (<div className='activities' key={item.id}>
+          <span className='activities-span'>{item.name}</span>
+          </div>))}</div>) : <></> }
+        {/* Show just games */}
       {filtered === 'Games' ? (<div>{activities.filter(
         active => active.type === 'Games').map(item => 
-        (<div key={item.id}>{item.name}</div>))}</div>) : <></> }
+          (<div className='activities' key={item.id}>
+          <span className='activities-span'>{item.name}</span>
+          </div>))}</div>) : <></> }
+        {/* Show just Informational */}
       {filtered === 'Informational' ? (<div>{activities.filter(
         active => active.type === 'Informational').map(item => 
-        (<div key={item.id}>{item.name}</div>))}</div>) : <></> }
+          (<div className='activities' key={item.id}>
+          <span className='activities-span'>{item.name}</span>
+          </div>))}</div>) : <></> }
+        {/* Show just Video */}
       {filtered === 'Video' ? (<div>{activities.filter(
         active => active.type === 'Video').map(item => 
-        (<div key={item.id}>{item.name}</div>))}</div>) : <></> }
-      {/* {filtered === 'All' ? (<div>{activities.filter(type => type === 'All').map(filteredType => {
-       (
-          <div key={filteredType.name}>{filteredType}a aaaaa</div> 
-
-        )
-      })
-      } </div> ) : 'not' } */}
-      </>
+          (<div className='activities' key={item.id}>
+          <span className='activities-span'>{item.name}</span>
+          </div>))}</div>) : <></> }
+          </div>
+      </div>
     );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+
+
+
+const navStyles = makeStyles((theme) => ({
+    nav: {
+      flexGrow: 1,
+    },
+    appBar: {
+        background: 'linear-gradient(45deg, #8559A0 30%, #c383eb 90%)',
+    },
+    title: {
+      flexGrow: 1,
+    },
+  }));
+
+  export default function Header() {
+      const classes = navStyles();
+    return (
+        <div className={classes.nav}>
+        <AppBar className={classes.appBar} position="static">
+          <Toolbar>
+            <Typography variant="h4" className={classes.title}>
+              Activities
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </div>
+    )
+}
+
+
